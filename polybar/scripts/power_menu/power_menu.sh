ee33b6c418bdb0178dc4d7391b0ed7e5422bdb59
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-06-01 14:32:50 +0100

    [polybar] -> Add `power_menu` script

---
+++ polybar/scripts/power_menu/power_menu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rofi -show p -modi p:$HOME/dotfiles/polybar/scripts/power_menu/rofi-power-menu \
+-theme $HOME/dotfiles/polybar/scripts/power_menu/style.rasi \
+-font "Fira Code 11" \
+-width 20 \
+-lines 6
