d9148ecdec6568c215faa928332cf79ca10c55d1
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-07-21 15:49:05 +0100

    [rofi] -> Move power menu script from place

--- rofi/power-menu/power-menu.sh
+++ rofi/power-menu/power-menu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rofi -show p -modi p:$HOME/dotfiles/polybar/scripts/rofi-power-menu \
+rofi -show p -modi p:$HOME/dotfiles/polybar/scripts/power-menu.config \
 -theme $HOME/dotfiles/rofi/power-menu/style.rasi \
 -font "Fira Code 11" \
 -width 20 \
