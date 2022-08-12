26000a8c6672457895535b1b29d894a73af138b0
RuiL1904 <rui.lilokas2003@hotmail.com>
2022-08-12 19:47:14 +0100

    [polybar] -> Change launcher to handle both monitors

--- polybar/launcher.sh
+++ polybar/launcher.sh
@@ -8,3 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Lauch polybar
 polybar main -c $(dirname $0)/config.ini &
+
+case "$(xrandr -q | grep -w connected | wc -l)" in
+
+    2)  polybar second -c $(dirname $0)/config.ini &
+        ;;
+
+esac
