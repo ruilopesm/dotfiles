647cce6913b8b3ab520dd5a58f6c43888a5b9b44
Rui Lopes <ruilopesm19@gmail.com>
2024-12-27 21:01:37 +0000

    [elixir] -> redo config

--- elixir/.iex.exs
+++ elixir/.iex.exs
@@ -1,17 +1,7 @@
-clear()
+Logger.configure_backend(:console, device: Process.group_leader())
 
-IEx.configure(
-  colors: [
-    eval_result: [:green, :bright] ,
-    eval_error: [:red, :bright],
-    eval_info: [:yellow, :bright],
- ],
- default_prompt: [
-  "\e[G",
-  :red,
-  "|%counter|",
-  :blue,
-  ">>>",
-  :reset
-  ] |> IO.ANSI.format |> IO.chardata_to_string
-)
+defmodule :_utils do
+  defdelegate q(), to: System, as: :halt
+end
+
+import :_utils
